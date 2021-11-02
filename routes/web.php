1900232a65e8908ee619be16870f1520b2fa141f
daudi44 <daudi@iesebre.com>
2021-11-02 13:16:35 +0100

    Refactoritzar la vista tasks per utilitzar models laravel.

--- routes/web.php
+++ routes/web.php
@@ -1,64 +1,41 @@
 <?php
 
+use App\Models\TaskP;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $task1 = new stdClass();
-    $task1->id=1;
-    $task1->title="Tasca 1";
-    $task1->description="Sortir de LATAM";
-    $task1->completed=1;
-
-    $task2 = new stdClass();
-    $task2->id=2;
-    $task2->title="Tasca 2";
-    $task2->description="Invadir Pakistan";
-    $task2->completed=1;
-
-    $task3 = new stdClass();
-    $task3->id=3;
-    $task3->title="Tasca 3";
-    $task3->description="Escapar de classe de fol";
-    $task3->completed=1;
-
-    $tasks = [
-        $task1,
-        $task2,
-        $task3
-    ];
-
     return view('tasks', [
-        'tasks' => $tasks
+        'tasks' => TaskP::all()
     ]);
 });
 
 Route::get('/tasks', function () {
-    $task1 = new stdClass();
-    $task1->id=1;
-    $task1->title="Tasca 1";
-    $task1->description="Sortir de LATAM";
-    $task1->completed=1;
-
-    $task2 = new stdClass();
-    $task2->id=2;
-    $task2->title="Tasca 2";
-    $task2->description="Invadir Pakistan";
-    $task2->completed=1;
-
-    $task3 = new stdClass();
-    $task3->id=3;
-    $task3->title="Tasca 3";
-    $task3->description="Escapar de classe de fol";
-    $task3->completed=1;
-
-    $tasks = [
-        $task1,
-        $task2,
-        $task3
-    ];
+//    $task1 = new stdClass();
+//    $task1->id=1;
+//    $task1->title="Tasca 1";
+//    $task1->description="Sortir de LATAM";
+//    $task1->completed=1;
+//
+//    $task2 = new stdClass();
+//    $task2->id=2;
+//    $task2->title="Tasca 2";
+//    $task2->description="Invadir Pakistan";
+//    $task2->completed=1;
+//
+//    $task3 = new stdClass();
+//    $task3->id=3;
+//    $task3->title="Tasca 3";
+//    $task3->description="Escapar de classe de fol";
+//    $task3->completed=1;
+//
+//    $tasks = [
+//        $task1,
+//        $task2,
+//        $task3
+//    ];
 
     return view('tasks', [
-        'tasks' => $tasks
+        'tasks' => TaskP::all()
     ]);
 });
 
