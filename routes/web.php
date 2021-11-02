27022d131909496a481b99526238217f4d62fcb6
daudi44 <daudi@iesebre.com>
2021-11-02 12:45:14 +0100

    Refactoritzar la vista tasks per utilitzar laravel blade. {{}}

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $task1 = new stdClass();
-    $task1->name="Tasca 1";
+    $task1->id=1;
+    $task1->title="Tasca 1";
     $task1->description="Sortir de LATAM";
     $task1->completed=1;
 
     $task2 = new stdClass();
-    $task2->name="Tasca 2";
+    $task2->id=2;
+    $task2->title="Tasca 2";
     $task2->description="Invadir Pakistan";
     $task2->completed=1;
 
+    $task3 = new stdClass();
+    $task3->id=3;
+    $task3->title="Tasca 3";
+    $task3->description="Escapar de classe de fol";
+    $task3->completed=1;
+
     $tasks = [
         $task1,
-        $task2
+        $task2,
+        $task3
     ];
 
     return view('tasks', [
@@ -36,9 +45,16 @@ Route::get('/tasks', function () {
     $task2->description="Invadir Pakistan";
     $task2->completed=1;
 
+    $task3 = new stdClass();
+    $task3->id=3;
+    $task3->title="Tasca 3";
+    $task3->description="Escapar de classe de fol";
+    $task3->completed=1;
+
     $tasks = [
         $task1,
-        $task2
+        $task2,
+        $task3
     ];
 
     return view('tasks', [
